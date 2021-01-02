9476da5c6461eb288362d63424683af139c2608f
TdWa <59452572+TdWa@users.noreply.github.com>
2021-01-02 01:03:25 +0100

    rename answer to part

--- day1/day1.js
+++ day1/day1.js
@@ -3,7 +3,7 @@ const nums = require("./input1");
 loop1: for (let a = 0; a < nums.length; a++) {
   for (let b = a + 1; b < nums.length; b++) {
     if (nums[a] + nums[b] === 2020) {
-      console.log("answer one:", nums[a] * nums[b]);
+      console.log("part one:", nums[a] * nums[b]);
       break loop1;
     }
   }
@@ -13,7 +13,7 @@ loop1: for (let a = 0; a < nums.length; a++) {
   for (let b = a + 1; b < nums.length; b++) {
     for (let c = b + 1; c < nums.length; c++) {
       if (nums[a] + nums[b] + nums[c] === 2020) {
-        console.log("answer two:", nums[a] * nums[b] * nums[c]);
+        console.log("part two:", nums[a] * nums[b] * nums[c]);
         break loop1;
       }
     }
